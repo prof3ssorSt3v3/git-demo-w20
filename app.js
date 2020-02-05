b5be84b95663d71ce28ed31cd1a97bbda3eaa4a8
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2020-02-04 21:24:10 -0500

    require json files

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
+const friends = require("./friends.json");
+const family = require("./family.json");
+
 const app = {
   init: () => {
     console.log("app running");
-  },
-  one: 1,
-  two: 2,
-  three: 3,
-  four: 4,
-  five: 5,
-  six: 6
+  }
 };
 
 setTimeout(app.init, 1000);
